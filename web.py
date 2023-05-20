41f6ceea14faa63b8ca3be05d84c47466a67e247
Justin-Vision <97095919+Justindescode@users.noreply.github.com>
2023-05-20 15:55:33 -0500

    Static Web completed now working on the deployment :)

--- web.py
+++ web.py
@@ -17,11 +17,16 @@ st.subheader("I hope you enjoy this app as much as I enjoyed developing it!:)")
 st.write("This app is to increase your productivity. ")
 
 
-for todo in todos:
-    option = st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
+
 
 
 st.text_input(label="", placeholder="Add a new todo....",
               on_change=add_todo, key='new_todo')
 
-st.session_state
